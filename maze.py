e408686e1ed14527a75434795698dab2a37a72b7
5tuartw <stuwilson@gmail.com>
2025-01-30 13:26:51 +0000

    Update maze.py break entrance and exit

--- maze.py
+++ maze.py
@@ -21,7 +21,10 @@ class Maze():
     self._cell_size_y = cell_size_y
     self._win = win
     self._cells = []
+    
     self._create_cells()
+    self._break_entrance_and_exit()
+    
 
   def _create_cells(self):
     for column in range(self._num_cols):
@@ -49,5 +52,11 @@ def _animate(self):
     return
   self._win.redraw()
   time.sleep(0.05)
+
+def _break_entrance_and_exit(self):
+  self._cells[o][o].has_top_wall = False
+  self._draw_cell(0,0)
+  self._cells[_num_cols-1][_num_rows-1].has_bottom_wall = False
+  self._draw_cell(_num_cols-1, _num_rows-1)
   
         
