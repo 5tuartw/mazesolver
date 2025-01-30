6497b8ca06a19ef1bb46db903f9d65b5ae81301c
5tuartw <stuwilson@gmail.com>
2025-01-30 13:20:34 +0000

    Update main.py for drawing maze

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graphics import Window, Point, Line
+from cell import Cell
 
 def main():
-    win = Window(800, 600)
     
     cell1 = Cell(win)
     cell1.has_left_wall = False
@@ -18,9 +18,20 @@ def main():
     cell4 = Cell(win)
     cell4.has_bottom_wall = False
     cell4.draw(700,500,750,550)
+
+    num_rows = 8
+    num_cols = 12
+    margin = 25
+    screen_x = 800
+    screen_y = 600
+    
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     
     
     win.wait_for_close()
 
-if __name__ == "__main__":
-    main()
+main()
