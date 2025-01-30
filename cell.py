97278c5c0e21b9eb9c49c268cde98b2c68c08e49
5tuartw <stuwilson@gmail.com>
2025-01-30 13:29:39 +0000

    Update cell.py to draw white walls if missing

--- cell.py
+++ cell.py
@@ -16,13 +16,25 @@ class Cell():
     if self.has_left_wall:
       line = Line(Point(x1,y1), Point(x1,y2))
       self._win.draw_line(line)
+    else:
+      line = Line(Point(x1,y1), Point(x1,y2))
+      self._win.draw_line(line, "white")      
     if self.has_right_wall:
+      line = Line(Point(x2,y1), Point(x2,y2))
+      self._win.draw_line(line)
+    else:
       line = Line(Point(x2,y1), Point(x2,y2))
       self._win.draw_line(line)
     if self.has_top_wall:
       line = Line(Point(x1,y1), Point(x1,y1))
       self._win.draw_line(line)
+    else:
+      line = Line(Point(x1,y1), Point(x1,y1))
+      self._win.draw_line(line, "white")
     if self.has_bottom_wall:
+      line = Line(Point(x1,y2), Point(x2,y2))
+      self._win.draw_line(line)
+    else:
       line = Line(Point(x1,y2), Point(x2,y2))
       self._win.draw_line(line)
 
